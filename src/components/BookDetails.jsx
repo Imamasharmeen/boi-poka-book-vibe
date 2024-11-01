55b2da294d15234289d4e85d40e6ad43175f61f8
Imamasharmeen <imamasharmeen@gmail.com>
2024-11-01 11:43:36 +0200

    set key in tje books component and load data for bookDetails and find the selected bookId in the book details

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useLoaderData, useParams } from 'react-router-dom'
 
 export default function BookDetails() {
+    const { bookId }= useParams()
+    const data = useLoaderData()
+    const id= parseInt(bookId)
+    //console.log(data)
+    const book=data.find(book=> book.bookId === id)
   return (
     <div>
-        <h1>BookDetaILS</h1>
+        <h1>BookDetaILS:{bookId}</h1>
     </div>
   ) 
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -8,11 +8,12 @@ export default function Books() {
         .then(res=>res.json())
         .then(data=>setBooks(data))
     },[])
+   
     return (
         <div>
             <div className='w-10/12 mx-auto border grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    books.map(book =><Book book={book}></Book>)
+                    books.map(book =><Book key={book.bookId} book={book}></Book>)
                 }
             </div>
         </div>
